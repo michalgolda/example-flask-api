437e65a44cd31d729d07d65136c4563cb7249d63
michalgolda <michal.golda@hotmail.com>
2024-09-14 10:49:23 +0200

    fix: name variable is not defined

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from services.greeting import GreetingService
 app = Flask("example-flask-api")
 
 @app.route("/greeting/<name>", methods=["GET"])
-def greeting():
+def greeting(name: str):
     service = GreetingService()
     service_output = service.execute(name)
     return service_output
